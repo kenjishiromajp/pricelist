70ee8afcef958be5c92081d9fadd71dc78d94560
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-11-27 21:05:06 +1000

    Updates

---
+++ precache-manifest.d3f73fe1413d74169e54f9ac17ea2abe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00fd058d1441ce400d76",
+    "url": "/pricelist/static/js/runtime~main.00fd058d.js"
+  },
+  {
+    "revision": "eb0eca2294cb922c1485",
+    "url": "/pricelist/static/js/main.eb0eca22.chunk.js"
+  },
+  {
+    "revision": "f76044e71ddca047718d",
+    "url": "/pricelist/static/js/1.f76044e7.chunk.js"
+  },
+  {
+    "revision": "eb0eca2294cb922c1485",
+    "url": "/pricelist/static/css/main.77e9a41a.chunk.css"
+  },
+  {
+    "revision": "3c1e0d104c1594cc2f1852c78f57eec4",
+    "url": "/pricelist/index.html"
+  }
+];
